eb0ceb399ae0eff055d94f546f7667049c4cb1c4
Arezoo <arezooinfo883@gmail.com>
2024-07-30 14:14:16 +0100

    front page

---
+++ finalProject/storyPage1.swift
@@ -0,0 +1,18 @@
+//
+//  storyPage1.swift
+//  finalProject
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+struct storyPage1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    storyPage1()
+}
---
+++ finalProjectTests/win.swift
@@ -0,0 +1,18 @@
+//
+//  win.swift
+//  finalProjectTests
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+struct win: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    win()
+}
